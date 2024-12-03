7e6ac805878da61ed698536a27d9b99ad64977d2
“Rihoko” <a1219814581@163.com>
2024-12-03 05:22:59 +0000

    oo

--- detect/main.py
+++ detect/main.py
@@ -1,71 +1,86 @@
-from machine import Pin, PWM
-from servo import Servo
+import cv2  # 导入 OpenCV 库
+import time  # 导入时间库
+from detector import ArmorDetector  # 从 detector 导入 Detector 类
+from transfer import Trans
+from armor_tracker_node import ArmorTracker
+from loguru import logger
 import time
-from machine import UART
-import json
-import _thread
 
-time.sleep(1)  # 防止点停止按钮后马上再启动导致 Thonny 连接不上
+class Cam():
+    def __init__(self, cam_params):    
+        self.width = cam_params["width"]  # 你想要的宽度
+        self.height = cam_params["height"]   # 你想要的高度
+        self.fps = cam_params["fps"]  # 你想要的帧率
+        self.cam_num = cam_params["cam_num"]
+    def detect(self, detector, tracker, transfer):
+        tracker.pic_width = self.width
+        video_stream = cv2.VideoCapture(self.cam_num)  # 打开视频流
+                # 设置分辨率
+        video_stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        video_stream.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        video_stream.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        # 设置帧率
+        video_stream.set(cv2.CAP_PROP_FPS, self.fps)
+        w = video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)
+        h = video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        fps = video_stream.get(cv2.CAP_PROP_FPS)
 
-servo_yaw = Servo(1, limit_min_angle = 90-45, limit_max_angle = 90+45)
-servo_pitch = Servo(0, limit_min_angle = 90-45, limit_max_angle = 90+25)
-
-uart = UART(1, 115200, rx=21, tx=20)  # 设置串口号1和波特率
-
-kp_yaw = 0.8
-kp_pitch = 0.6
-
-speed_yaw = 0.000_0001
-speed_pitch = 0.000_0001
-
-def run(callback):
-    global speed_yaw, speed_pitch, kp_yaw, kp_pitch
+        if not video_stream.isOpened():  # 检查视频流是否成功打开
+            print("错误: 无法打开视频流。")
+        while True:  # 持续读取视频帧
+            start_time = time.time()  # 记录帧处理开始时间
+            ret, frame = video_stream.read()  # 读取视频帧
+            if not ret:  # 如果未成功读取帧
+                print("错误: 无法读取帧")
+                break  # 退出循环
+            #logger.info(f"cam: {w} x {h} @ {fps}") 
+            info = detector.detect_armor(frame)  # 使用 detector 进行检测
+            target_yaw, target_pitch = tracker.track(info)
+            transfer.send(target_yaw, target_pitch)
+            end_time = time.time()  # 记录帧处理结束时间
+            detection_time = (end_time - start_time) * 1000  # 转换为毫秒
+            #logger.debug(f"检测延迟: {int(detection_time)} 毫秒")  # 输出检测延迟
+            if cv2.waitKey(1) & 0xFF == ord("q"):  # 检测按键
+                break  # 退出循环
+        video_stream.release()  # 释放视频流
+        cv2.destroyAllWindows()  # 关闭所有窗口
+        transfer.close()
+        logger.info("off")
     
-    while True:
-        # 判断有无收到信息
-        if uart.any():
-            time.sleep(0.01)
-            json_data = uart.read(uart.any())  # 接收可用数据
-            try:
-                # 解码并分割数据
-                data = json.loads(json_data)  # 解析 JSON 字符串为字典
-                yaw = data['yaw']  # 提取 yaw 值
-                pitch = data['pitch']  # 提取 pitch 值
-                print(f'Parsed Yaw: {yaw}, Parsed Pitch: {pitch}')  # 打印解析后的值
-                yaw = -yaw
-                pitch = -pitch
-                # servo_yaw.set_angle_relative(yaw * kp_yaw)  # 设置舵机角度
-                # servo_pitch.set_relative(pitch * kp_pitch)  # 设置舵机角度
-
-                speed_yaw = yaw * kp_yaw
-                speed_pitch = pitch * kp_pitch
-                
-                # 发送反馈信息
-                uart.write(f'{yaw}/{pitch}\n'.encode('utf-8'))  # 发送一条数据
-                time.sleep(0.08)
-            except ValueError as e:
-                print(f"数据格式错误: {e}")  # 捕获并打印格式错误
-            except IndexError:
-                print("接收到的数据格式不正确，无法提取 yaw 和 pitch。")
-
-def step_yaw(callback):
-    global speed_yaw, speed_pitch
-    while True:
-        servo_yaw.set_angle_relative(0.1)
-        time.sleep(1/speed_yaw)
-
-def step_pitch(callback):
-    global speed_yaw, speed_pitch
-    while True:
-        servo_yaw.set_angle_relative(0.1)
-        time.sleep(1/speed_pitch)
-
-_thread.start_new_thread(run,("1",)) #开启线程1,参数必须是元组
-_thread.start_new_thread(step_yaw,("2",)) #开启线程2，参数必须是元组
-_thread.start_new_thread(step_pitch,("3",)) #开启线程2，参数必须是元组
-
-
-
-
-
-
+detect_color =  0  # 颜色参数 0: 识别红色装甲板, 1: 识别蓝色装甲板, 2: 识别全部装甲板
+# 图像参数字典
+binary_val = 78  
+light_params = {
+    "light_area_min": 19,  # 最小灯条面积
+    "light_angle_min": -45,  # 最小灯条角度
+    "light_angle_max": 45,  # 最大灯条角度
+    "light_angle_tol": 20,  # 灯条角度容差
+    "vertical_discretization": 0.615,  # 垂直离散
+    "height_tol": 34,  # 高度容差
+    "cy_tol":24,  # 中心点的y轴容差
+    "height_multiplier": 3 
+}
+# 颜色参数字典
+color_params = {
+    "armor_color": {1: (255, 255, 0), 0: (128, 0, 128)},  # 装甲板颜色映射
+    "armor_id": {1: 1, 0: 7},  # 装甲板 ID 映射
+    "light_color": {1: (200, 71, 90), 0: (0, 100, 255)},  # 灯条颜色映射
+    "light_dot": {1: (0, 0, 255), 0: (255, 0, 0)}  # 灯条中心点颜色映射
+}
+cam_params = { 
+        "width": 640,  # 你想要的宽度
+        "height": 480,   # 你想要的高度
+        "fps": 180,  # 你想要的帧率
+        "cam_num": 1  # 摄像头编号
+}
+# 配置串口参数
+serial_port = '/dev/ttyS2'  # 根据实际情况修改
+baud_rate = 115200       # 波特率
+timeout = 1            # 超时设置
+detector = ArmorDetector(detect_color, 0, binary_val, light_params, color_params)  # 创建检测器对象
+tracker = ArmorTracker(detect_color)
+tracker.frame_add = 5
+tracker.vfov = 36
+transfer = Trans(serial_port, baud_rate, timeout)
+cam = Cam(cam_params)
+cam.detect(detector, tracker, transfer)

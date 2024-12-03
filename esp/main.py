d9de94fe3436ad6033be2d07ea00e798b10fe063
“Rihoko” <a1219814581@163.com>
2024-12-03 18:05:53 +0800

    refactor(detect): 优化装甲板检测与跟踪算法
    - 调整 ArmorTracker 类中中心点和高度的初始化值
    - 修改检测和跟踪算法参数，提高

--- esp/main.py
+++ esp/main.py
@@ -15,54 +15,33 @@ uart = UART(1, 115200, rx=21, tx=20)  # 设置串口号1和波特率
 kp_yaw = 0.8
 kp_pitch = 0.6
 
-speed_yaw = 0.000_0001
-speed_pitch = 0.000_0001
 
-def run(callback):
-    global speed_yaw, speed_pitch, kp_yaw, kp_pitch
-    
-    while True:
-        # 判断有无收到信息
-        if uart.any():
-            time.sleep(0.01)
-            json_data = uart.read(uart.any())  # 接收可用数据
-            try:
-                # 解码并分割数据
-                data = json.loads(json_data)  # 解析 JSON 字符串为字典
-                yaw = data['yaw']  # 提取 yaw 值
-                pitch = data['pitch']  # 提取 pitch 值
-                print(f'Parsed Yaw: {yaw}, Parsed Pitch: {pitch}')  # 打印解析后的值
-                yaw = -yaw
-                pitch = -pitch
-                # servo_yaw.set_angle_relative(yaw * kp_yaw)  # 设置舵机角度
-                # servo_pitch.set_relative(pitch * kp_pitch)  # 设置舵机角度
+while True:
+    # 判断有无收到信息
+    if uart.any():
+        time.sleep(0.01)
+        json_data = uart.read(uart.any())  # 接收可用数据
+        try:
+            # 解码并分割数据
+            data = json.loads(json_data)  # 解析 JSON 字符串为字典
+            yaw = data['yaw']  # 提取 yaw 值
+            pitch = data['pitch']  # 提取 pitch 值
+            print(f'Parsed Yaw: {yaw}, Parsed Pitch: {pitch}')  # 打印解析后的值
+            yaw = -yaw
+            pitch = -pitch
+            servo_yaw.set_angle_relative(yaw * kp_yaw)  # 设置舵机角度
+            servo_pitch.set_angle_relative(pitch * kp_pitch)  # 设置舵机角度
+            
+            # 发送反馈信息
+            uart.write(f'{yaw}/{pitch}\n'.encode('utf-8'))  # 发送一条数据
+            time.sleep(0.08)
+        except ValueError as e:
+            print(f"数据格式错误: {e}")  # 捕获并打印格式错误
+        except IndexError:
+            print("接收到的数据格式不正确，无法提取 yaw 和 pitch。")
 
-                speed_yaw = yaw * kp_yaw
-                speed_pitch = pitch * kp_pitch
-                
-                # 发送反馈信息
-                uart.write(f'{yaw}/{pitch}\n'.encode('utf-8'))  # 发送一条数据
-                time.sleep(0.08)
-            except ValueError as e:
-                print(f"数据格式错误: {e}")  # 捕获并打印格式错误
-            except IndexError:
-                print("接收到的数据格式不正确，无法提取 yaw 和 pitch。")
 
-def step_yaw(callback):
-    global speed_yaw, speed_pitch
-    while True:
-        servo_yaw.set_angle_relative(0.1)
-        time.sleep(1/speed_yaw)
 
-def step_pitch(callback):
-    global speed_yaw, speed_pitch
-    while True:
-        servo_yaw.set_angle_relative(0.1)
-        time.sleep(1/speed_pitch)
-
-_thread.start_new_thread(run,("1",)) #开启线程1,参数必须是元组
-_thread.start_new_thread(step_yaw,("2",)) #开启线程2，参数必须是元组
-_thread.start_new_thread(step_pitch,("3",)) #开启线程2，参数必须是元组
 
 
 
